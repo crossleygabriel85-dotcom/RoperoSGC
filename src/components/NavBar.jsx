d2fdf9294da0e68b8ab506be5ec3644f59f6382b
crossleygabriel85-dotcom <crossleygabriel85@gmail.com>
2026-09-01 18:30:05 -0400

    Remove NavBar component content per user request

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,2 @@
-import React from 'react';
-import './NavBar.css';
-import logo from '../images/logo.png'; // Coloca aquí el logo: src/images/logo.png
-
-export default function NavBar() {
-  return (
-    <a className="brand" href="/">
-      Ropero <img src={logo} alt="Colegio Saint George" className="logo" />
-    </a>
-  );
-}
+// NavBar component removed per user request
+export default null;
